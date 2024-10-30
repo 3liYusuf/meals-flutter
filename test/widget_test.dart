bbd52966f2261b2629585f92173f159b1119dc5f
Ali Yusuf <alcndb@gmail.com>
2024-10-30 14:40:47 +0300

    const

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,7 +10,7 @@ void main() {
     await tester.pumpWidget(
       const ProviderScope(
         child: MaterialApp(
-          home: const TabsScreen(),
+          home: TabsScreen(),
         ),
       ),
     );
